b1a9f9f5c45556c8f2c118ec6835d4b50bd3915c
Woody <gintooooonic@gmail.com>
2023-03-29 21:40:21 +0900

    build: MUI 의존성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
 
+// MUI를 위한 Roboto 폰트 로드
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <div className="App">
