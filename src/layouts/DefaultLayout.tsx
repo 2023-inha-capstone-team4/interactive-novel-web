dbb9df3e3592f545bcebcd307fca081345fdf793
Woody <gintooooonic@gmail.com>
2023-04-01 00:35:52 +0900

    feat: 앱 최대 너비 700px로 제한

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,18 +1,33 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import styled from '@emotion/styled';
 
 /**
  * 기본 페이지 레이아웃입니다.
  */
 function DefaultLayout() {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <Background>
+      <Content>
+        <Header />
+        <Outlet />
+        <Footer />
+      </Content>
+    </Background>
   );
 }
+
+const Background = styled.div`
+  background-color: #f2f2f2;
+`;
+
+const Content = styled.div`
+  height: 100vh;
+  max-width: 700px;
+  margin: 0 auto;
+  background-color: #ffffff;
+  overflow-y: scroll;
+`;
 
 export default DefaultLayout;
