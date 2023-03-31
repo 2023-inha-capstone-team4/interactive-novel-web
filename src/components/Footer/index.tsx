9c6074447e3c248f65081c9682e55b6f0d0e8bab
Woody <gintooooonic@gmail.com>
2023-04-01 00:26:51 +0900

    feat: Footer 내용 추가

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,25 @@
+import styled from '@emotion/styled';
+
 /**
  * 페이지의 Footer 요소입니다.
  */
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <StyledFooter>
+      <p>Interactive Novel</p>
+      <a href="mailto:vivalavida@inha.edu">문의</a>
+    </StyledFooter>
+  );
 }
+
+const StyledFooter = styled.footer`
+  margin-top: 50px;
+  text-align: center;
+
+  & > * {
+    font-size: 10px;
+    color: #424242;
+  }
+`;
 
 export default Footer;
