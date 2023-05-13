a12af7537f25a50933053ed3afb81d3f1c3087bb
Woody <gintooooonic@gmail.com>
2023-05-13 18:13:47 +0900

    feat: 마이페이지 버튼에서 로그인된 경우 프로필 이미지 출력

--- src/components/MyAccountButton/index.tsx
+++ src/components/MyAccountButton/index.tsx
@@ -1,16 +1,26 @@
-import { IconButton } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import LoginIcon from '@mui/icons-material/Login';
 import PersonIcon from '@mui/icons-material/Person';
 import { findAccessToken } from '../../services/auth-service';
+import { useEffect, useState } from 'react';
+import { User } from '../../types/User';
+import UserAPI from '../../api/UserAPI';
 
 export default function MyAccountButton() {
   const navigate = useNavigate();
 
-  const accessToken = findAccessToken();
+  // 상태: 내 정보
+  const [info, setInfo] = useState<User>();
 
+  /**
+   * 버튼 클릭에 대한 핸들러입니다.
+   *
+   * 조회된 내 정보가 존재하는 경우에는 로그인이 된 것으로 간주하여 마이 페이지로,
+   * 그렇지 않은 경우에는 로그인 화면으로 이동시킵니다.
+   */
   const handleAccountButtonClick = () => {
-    if (!accessToken) {
+    if (!info) {
       navigate('/sign/in');
       return;
     }
@@ -18,9 +28,18 @@ export default function MyAccountButton() {
     navigate('/my');
   };
 
+  // 내 계정 정보를 불러옵니다.
+  useEffect(() => {
+    UserAPI.findMyInfo().then((resp) => setInfo(resp.data));
+  });
+
   return (
     <IconButton onClick={handleAccountButtonClick}>
-      {accessToken ? <PersonIcon /> : <LoginIcon />}
+      {info ? (
+        <Avatar src={info.imageUrl} alt="profile" sx={{ width: 24, height: 24 }} />
+      ) : (
+        <LoginIcon />
+      )}
     </IconButton>
   );
 }
